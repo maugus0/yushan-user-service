92ad79b5c2fd12fa8a55b7e7867f40c767c97d36
luxury <1784304095@qq.com>
2025-10-24 15:29:34 +0800

    [YM-141] fix

--- src/main/resources/db/migration/V3__Admin_seed_data.sql
+++ src/main/resources/db/migration/V3__Admin_seed_data.sql
@@ -3,4 +3,4 @@ INSERT INTO users (
     profile_detail, birthday, gender, is_admin, is_author,
     status, create_time, update_time, last_login, last_active
 ) VALUES
-(gen_random_uuid(), 'admin@yushan.com', 'admin', '$2a$10$ho2svjNlaADthonvqDjOcuLHp0tHbQ6r282MtEchmpuX52JouDIxC', 'user_male.png', 'Admin', '1985-03-15', 0, true, true, 0, CURRENT_TIMESTAMP - INTERVAL '60 days', CURRENT_TIMESTAMP - INTERVAL '1 day', CURRENT_TIMESTAMP - INTERVAL '2 hours', CURRENT_TIMESTAMP - INTERVAL '30 minutes'),
+(gen_random_uuid(), 'admin@yushan.com', 'admin', '$2a$10$ho2svjNlaADthonvqDjOcuLHp0tHbQ6r282MtEchmpuX52JouDIxC', 'user_male.png', 'Admin', '1985-03-15', 0, true, true, 0, CURRENT_TIMESTAMP - INTERVAL '60 days', CURRENT_TIMESTAMP - INTERVAL '1 day', CURRENT_TIMESTAMP - INTERVAL '2 hours', CURRENT_TIMESTAMP - INTERVAL '30 minutes');
